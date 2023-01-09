9f2ae2b88270258c0721be5be23744dc2381788b
syed-zunair-gillani <gillanix007@gmail.com>
2023-01-09 18:16:25 +0500

    residence single page update

--- pages/residences/[slug].jsx
+++ pages/residences/[slug].jsx
@@ -5,12 +5,13 @@ import { PageBanner } from "../../src/components";
 import { HiArrowNarrowLeft } from "react-icons/hi";
 import { heroCard } from "../../public";
 import Image from "next/image";
+import { One, Two, Three, Four } from "../../public";
 
 const Slug = () => {
   return (
     <>
       <PageBanner src={ResidenceBannerImage} />
-      <section className="mt-9 container mx-auto p-1 md:p-0 mb-4">
+      <section className="mt-9 container mx-auto p-2 md:p-0 mb-5 md:mb-10">
         <Link href="/residences">
           <h1 className="text-sm font-bangla-mn flex capitalize">
             <HiArrowNarrowLeft size={14} className="-mt-[0.001] mr-1" /> Nos
@@ -21,23 +22,32 @@ const Slug = () => {
           Nos Résidences
         </h1>
         {/* Gallery */}
-        <div className="grid grid-cols-5 gap-4 mt-3">
+        <div className="grid md:grid-cols-3 lg:grid-cols-5 gap-4 mt-3 ">
+        {/* 1 */}
           <div className="max-h-[700px] col-span-2">
-            <Image src={heroCard} alt="" className="w-full h-full object-cover rounded-3xl"/>
+            <Image src={One} alt="" className="w-full h-full object-cover rounded-3xl"/>
           </div>
-          <div className="max-h-[700px]">
-            <div className="h-[40%]"> <Image src={heroCard} alt="" className="w-full h-full object-cover rounded-3xl"/> </div>
-            <div className="h-[60%] pt-6"> <Image src={heroCard} alt="" className="w-full h-full object-cover rounded-3xl"/> </div>
+           {/* 2 */}
+          <div className="md:max-h-[700px] col-span-2 md:col-span-1">
+            <div className="h-[40%]"> <Image src={Two} alt="" className="w-full h-full object-cover rounded-3xl"/> </div>
+            <div className="h-[60%] pt-4 md:pt-6"> <Image src={Three} alt="" className="w-full h-full object-cover rounded-3xl"/> </div>
           </div>
-          <div className=" col-span-2 max-h-[700px]">
-            <div className="h-[50%]"> <Image src={heroCard} alt="" className="w-full h-full object-cover rounded-3xl"/> </div>
-            <div className="h-[50%] pt-6">
-              <h6 className="font-normal text-xl"><strong className="text-4xl font-medium">149 €</strong> <sub>par nuit</sub></h6>
-              <div>
+           {/* 3 */}
+          <div className="col-span-2 md:col-span-3 lg:col-span-2 md:max-h-[700px]">
+            <div className="md:h-[50%]"> <Image src={Four} alt="" className="w-full h-full object-cover rounded-3xl"/> </div>
+            <div className="md:h-[50%] pt-6 p-6">
+              <h6 className=" text-xl"><strong className="text-4xl font-semibold">149 €</strong> <sub>par nuit</sub></h6>
+              <div className="mt-6 border-b-2 border-gray-200 pb-4">
                 <h5 className="font-bold text-[22px]">Arrivée</h5>
-                <p className="text-sm font-semibold">Quand voulez vous arrivez ?</p>
+                <p className="text-sm font-normal text-gray-300">Quand voulez vous arrivez ?</p>
               </div>
-            </div>
+              <div className="mt-6 pb-4">
+                <h5 className="font-bold text-[22px]">Départ</h5>
+                <p className="text-sm font-normal text-gray-300">Quand voulez vous partir ? </p>
+              </div>
+              <button className="border w-full p-2 mt-4 border-black rounded-full text-base font-semibold hover:bg-black hover:text-white">réserver maintenant</button>
+              <h6 className="text-sm font-semibold mt-4 text-center cursor-pointer">+ de détails</h6>
+              </div>
           </div>
         </div>
       </section>
--- public/index.js
+++ public/index.js
@@ -1,2 +1,6 @@
 export {default as MainBanner} from './images/main.png'
 export {default as heroCard} from './images/herocard.png'
+export {default as One} from './images/one.png'
+export {default as Two} from './images/two.png'
+export {default as Three} from './images/three.png'
+export {default as Four} from './images/four.png'
