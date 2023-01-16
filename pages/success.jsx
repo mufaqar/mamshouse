044ab5843e838057cbec163a575aa2f46c0f2296
syed-zunair-gillani <gillanix007@gmail.com>
2023-01-16 19:58:13 +0500

    update

--- pages/success.jsx
+++ pages/success.jsx
@@ -10,26 +10,8 @@ const Success = () => {
     
 // },[])
 
-  const saveOrder=()=>{
-    var item_value = sessionStorage.getItem("item");
-    var items = JSON.parse(item_value)
-    console.log("🚀 ~ file: success.jsx:6 ~ useEffect ~ item_value", items)
-    const {title, getStartDate, getEndDate, totalprice} = items
 
-    axios .post("/api/createOrder", {
-      title,
-      getStartDate,
-      getEndDate,
-      totalprice,
-    })
-    .then(function (response) {
-      console.log("response", response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  }
-  saveOrder()
+
 
   return (
     <div className='mt-20'>Success</div>
