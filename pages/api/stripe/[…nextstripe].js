882668564141af38d639afd55bec011d6b0a4f27
Syed Zunair Gillani <gillanix007@gmail.com>
2023-01-17 14:53:28 +0500

    up

---
+++ pages/api/stripe/[…nextstripe].js
@@ -0,0 +1,4 @@
+import NextStripe from 'next-stripe'
+export default NextStripe({
+     stripe_key: "sk_test_51KZccKBYpJVF6ADtJIF54auA6RHJEEiEBasxyMMN8hvwOC2czH4rSBdt0tRCwCMw9gYUxynchdG5yjxCjYp44JyF00tvx0T4gJ"
+ })
