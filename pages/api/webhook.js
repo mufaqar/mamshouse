3a7bae34f2c1194a96909d02c9df8b2c38b49f78
Syed Zunair Gillani <gillanix007@gmail.com>
2023-01-17 14:11:42 +0500

    update

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,51 +1,23 @@
-import Stripe from 'stripe';
-import { buffer } from 'micro';
-import sanityClient from "@sanity/client";
 
-const stripe = new Stripe('sk_test_51KZccKBYpJVF6ADtJIF54auA6RHJEEiEBasxyMMN8hvwOC2czH4rSBdt0tRCwCMw9gYUxynchdG5yjxCjYp44JyF00tvx0T4gJ');
 
-const STRIPE_WEBHOOK_SECRET = "we_1MR9UXBYpJVF6ADtdQZXA5km"
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
 
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
+import { buffer } from "micro";
+import Stripe from "stripe";
+
+const stripe = new Stripe(
+  'sk_test_51KZccKBYpJVF6ADtJIF54auA6RHJEEiEBasxyMMN8hvwOC2czH4rSBdt0tRCwCMw9gYUxynchdG5yjxCjYp44JyF00tvx0T4gJ', {
+  apiVersion: "2020-08-27",
+});
+const webhookSecret = "we_1MR9UXBYpJVF6ADtdQZXA5km";
+
+const handler = async (req, res) => {
+  if (req.method === "POST") {
+    const buf = await buffer(req);
+    const sig = req.headers["stripe-signature"];
+
     let event;
 
-    try {
-      // 1. Retrieve the event by verifying the signature using the raw body and secret
-      const rawBody = await buffer(req);
-      const signature = req.headers['stripe-signature'];
-
-      event = stripe.webhooks.constructEvent(
-        rawBody.toString(),
-        signature,
-        STRIPE_WEBHOOK_SECRET
-      );
-    } catch (err) {
-      console.log(`❌ Error message: ${err.message}`);
-      res.status(400).send(`Webhook Error: ${err.message}`);
-      return;
-    }
-
-    // Successfully constructed event
-    console.log('✅ Success:', event.id);
-
-    // 2. Handle event type (add business logic here)
-    if (event.type === 'checkout.session.completed') {
-
-    } else {
-      console.warn(`🤷‍♀️ Unhandled event type: ${event.type}`);
-      
-    }
-
-    // 3. Return a response to acknowledge receipt of the event.
-    res.json({ received: true });
-  } else {
 
     const config = {
       projectId: "5x5wnf9u",
@@ -65,12 +37,18 @@ export default async function handler(req, res) {
       return res.status(500).json({ message: `Couldn't submit comment`, err });
     }
 
-    
-    res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed');
+    try {
+      event = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
+    } catch (err) {
+      res.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    res.json({ received: true });
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
   }
-}
+};
 
-
-
-
+export default handler;
