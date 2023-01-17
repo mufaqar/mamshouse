0b642fb6a26f4e66be01a6af9a4aea6a3d5e2fe0
Syed Zunair Gillani <gillanix007@gmail.com>
2023-01-17 13:54:06 +0500

    update

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -40,28 +40,32 @@ export default async function handler(req, res) {
 
     } else {
       console.warn(`🤷‍♀️ Unhandled event type: ${event.type}`);
-      const config = {
-        projectId: "5x5wnf9u",
-        dataset: "production",
-        useCdn: false,
-        token:
-          "skrDAeBk3tz1Dr4Yo3DIZlARomfTD13JEjnkN23A1XPejoWGFYbnj2PHBvkCTZRFocFevdHmIEYMOEDOpIFkHFqsDPRcujOIm2y4SitswFsYx6hQYCq5LlD3dB2lgs4rP1RDIrHeejWr1m7yOcJnEEdKcpWRwh2ag1i93VgKIcQoB4rryeey",
-      };
-      const client = sanityClient(config);
-      try {
-        await client.create({
-          _type: "booking",
-          title: "weebhook",
-        });
-      } catch (err) {
-        console.error(err);
-        return res.status(500).json({ message: `Couldn't submit comment`, err });
-      }
+      
     }
 
     // 3. Return a response to acknowledge receipt of the event.
     res.json({ received: true });
   } else {
+
+    const config = {
+      projectId: "5x5wnf9u",
+      dataset: "production",
+      useCdn: false,
+      token:
+        "skrDAeBk3tz1Dr4Yo3DIZlARomfTD13JEjnkN23A1XPejoWGFYbnj2PHBvkCTZRFocFevdHmIEYMOEDOpIFkHFqsDPRcujOIm2y4SitswFsYx6hQYCq5LlD3dB2lgs4rP1RDIrHeejWr1m7yOcJnEEdKcpWRwh2ag1i93VgKIcQoB4rryeey",
+    };
+    const client = sanityClient(config);
+    try {
+      await client.create({
+        _type: "booking",
+        title: "weebhook",
+      });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ message: `Couldn't submit comment`, err });
+    }
+
+    
     res.setHeader('Allow', 'POST');
     res.status(405).end('Method Not Allowed');
   }
