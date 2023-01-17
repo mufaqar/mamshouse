9842e97e58b127c3567eeebbc5cad46ad84dcf03
Syed Zunair Gillani <gillanix007@gmail.com>
2023-01-17 14:32:47 +0500

    up

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,54 +1,45 @@
 
 
-
-
 import { buffer } from "micro";
-import Stripe from "stripe";
+const Stripe = require('stripe');
 
-const stripe = new Stripe(
-  'sk_test_51KZccKBYpJVF6ADtJIF54auA6RHJEEiEBasxyMMN8hvwOC2czH4rSBdt0tRCwCMw9gYUxynchdG5yjxCjYp44JyF00tvx0T4gJ', {
-  apiVersion: "2020-08-27",
+const stripe = new Stripe("sk_test_51KZccKBYpJVF6ADtJIF54auA6RHJEEiEBasxyMMN8hvwOC2czH4rSBdt0tRCwCMw9gYUxynchdG5yjxCjYp44JyF00tvx0T4gJ", {
+    apiVersion: '2020-08-27'
 });
-const webhookSecret = "we_1MR9UXBYpJVF6ADtdQZXA5km";
+const webhookSecret = "we_1MRBWZBYpJVF6ADtu9Ws7pHs";
+
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
+
 
 const handler = async (req, res) => {
+    if (req.method === "POST") {
+        const buf = await buffer(req);
+        const sig = req.headers["stripe-signature"];
 
-  const config = {
-    projectId: "5x5wnf9u",
-    dataset: "production",
-    useCdn: false,
-    token:
-      "skrDAeBk3tz1Dr4Yo3DIZlARomfTD13JEjnkN23A1XPejoWGFYbnj2PHBvkCTZRFocFevdHmIEYMOEDOpIFkHFqsDPRcujOIm2y4SitswFsYx6hQYCq5LlD3dB2lgs4rP1RDIrHeejWr1m7yOcJnEEdKcpWRwh2ag1i93VgKIcQoB4rryeey",
-  };
-  const client = sanityClient(config);
-  try {
-    await client.create({
-      _type: "booking",
-      title: "weebhook",
-    });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ message: `Couldn't submit comment`, err });
-  }
+        let stripeEvent;
 
+        try {
+            stripeEvent = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
+            console.log( 'stripeEvent', stripeEvent );
+        } catch (err) {
+            res.status(400).send(`Webhook Error: ${err.message}`);
+            return;
+        }
 
+        if ( 'checkout.session.completed' === stripeEvent.type ) {
+            const session = stripeEvent.data.object;
+            console.log( 'payment success', session );
+// Do something here on payment success, like update order etc.        }
 
-  if (req.method === "POST") {
-    const buf = await buffer(req);
-    const sig = req.headers["stripe-signature"];
-    let event;    
-    try {
-      event = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
-    } catch (err) {
-      res.status(400).send(`Webhook Error: ${err.message}`);
-      return;
+        res.json({ received: true });
+    } else {
+        res.setHeader("Allow", "POST");
+        res.status(405).end("Method Not Allowed");
     }
-
-    res.json({ received: true });
-  } else {
-    res.setHeader("Allow", "POST");
-    res.status(405).end("Method Not Allowed");
-  }
 };
 
 export default handler;
