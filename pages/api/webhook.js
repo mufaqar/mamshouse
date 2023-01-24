2ca7a83991785a6a8bc96e989c4327fb1dffc1c2
Syed Zunair Gillani <gillanix007@gmail.com>
2023-01-24 15:33:52 +0500

    update and remove string

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -7,7 +7,7 @@ export default async function handler (req, res) {
   // check the request method and ensure you only accept POST requests.
   try {
     const requestBuffer = await buffer(req);
-    const signature = req.headers['stripe-signature'] as string;
+    const signature = req.headers['stripe-signature'];
     const stripe = new Stripe('sk_test_51KZccKBYpJVF6ADtJIF54auA6RHJEEiEBasxyMMN8hvwOC2czH4rSBdt0tRCwCMw9gYUxynchdG5yjxCjYp44JyF00tvx0T4gJ', { apiVersion: null }); // version null sets the most recent API version
     const event =  stripe.webhooks.constructEvent(
       requestBuffer.toString(), // Stringify the request for the Stripe library
