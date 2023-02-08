34ced9268581f0580c23661d72836b4f0bf2379f
Mufaqar Islam <mufaqar@gmail.com>
2023-02-08 13:56:13 +0500

    mams house

--- sanity.config.js
+++ sanity.config.js
@@ -1,18 +1,15 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './src/schemas'
+import { defineConfig } from 'sanity';
+import { deskTool } from 'sanity/desk';
+import { visionTool } from '@sanity/vision';
+import { schemaTypes } from './src/schemas';
 
 export default defineConfig({
   name: 'default',
-  title: 'Sanity Project',
-
+  title: 'MamsHouse',
   projectId: '5x5wnf9u',
   dataset: 'production',
-
   plugins: [deskTool(), visionTool()],
-
   schema: {
     types: schemaTypes,
   },
-})
+});
