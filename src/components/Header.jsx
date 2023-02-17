d09f817db1d2f0b0b6e60658b778a62e49b3260f
Mufaqar Islam <mufaqar@gmail.com>
2023-02-17 18:20:32 +0500

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
-import { LanguageSwitcher } from "next-export-i18n";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useState } from "react";
-import { FaInstagram } from "react-icons/fa";
-import { FiMenu } from "react-icons/fi";
-import { GrClose } from "react-icons/gr";
+import { LanguageSwitcher } from 'next-export-i18n';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { FaInstagram } from 'react-icons/fa';
+import { FiMenu } from 'react-icons/fi';
+import { GrClose } from 'react-icons/gr';
 
 export default function Header() {
   const [isMobile, setIsMobile] = useState(false);
@@ -17,8 +17,8 @@ export default function Header() {
   const ChangeLang = (lang) => {
     const query = { ...router.query, lang };
     l > 0
-      ? router.push({ pathname: "/", query })
-      : router.push({ pathname: "/", query: { lang: lang } });
+      ? router.push({ pathname: '/', query })
+      : router.push({ pathname: '/', query: { lang: lang } });
   };
 
   const ChangeNav = (path) => {
@@ -38,53 +38,53 @@ export default function Header() {
         <div>
           <h1
             className="uppercase cursor-pointer mt-[14px] font-bangla-mn text-xl md:text-2xl leading-7"
-            onClick={() => ChangeNav("/")}
+            onClick={() => ChangeNav('/')}
           >
             mamshouse.
           </h1>
         </div>
-        <nav className={`md:block ${isMobile ? "block" : "hidden"}`}>
+        <nav className={`md:block ${isMobile ? 'block' : 'hidden'}`}>
           <ul
             className={`flex font-semibold text-sm gap-20 items-center ${
               isMobile &&
-              "bg-white shadow-sm p-8 gap-8 absolute top-16 z-10 w-full left-0 flex-col"
+              'bg-white shadow-sm p-8 gap-8 absolute top-16 z-10 w-full left-0 flex-col'
             }`}
           >
             <li
               className={`cursor-pointer ${
-                pathname === "/residences" && "nav_item"
+                pathname === '/residences' && 'nav_item'
               }`}
-              onClick={() => ChangeNav("residences")}
+              onClick={() => ChangeNav('residences')}
             >
               Résidences
             </li>
             <li
               className={`cursor-pointer ${
-                pathname === "/activities" && "nav_item"
+                pathname === '/activities' && 'nav_item'
               }`}
-              onClick={() => ChangeNav("activities")}
+              onClick={() => ChangeNav('activities')}
             >
               Activités
             </li>
             <li
               className={`cursor-pointer ${
-                pathname === "/contact" && "nav_item"
+                pathname === '/contact' && 'nav_item'
               }`}
-              onClick={() => ChangeNav("contact")}
+              onClick={() => ChangeNav('contact')}
             >
               Contact
             </li>
-            <li className={`ml-6 cursor-pointer text-lg ${isMobile && "ml-0"}`}>
-              <Link href="#">
+            <li className={`ml-6 cursor-pointer text-lg ${isMobile && 'ml-0'}`}>
+              <Link href="https://www.instagram.com/mamshouse" target="_blank">
                 <FaInstagram />
               </Link>
             </li>
             <div className="flex gap-4">
-              <div onClick={() => ChangeLang("fr")}>
+              <div onClick={() => ChangeLang('fr')}>
                 <div className="hover:underline cursor-pointer">FR</div>
               </div>
               |
-              <div onClick={() => ChangeLang("en")}>
+              <div onClick={() => ChangeLang('en')}>
                 <div className="hover:underline cursor-pointer">EN</div>
               </div>
             </div>
