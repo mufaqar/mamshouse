a107894f06b174c2d7e70d8f42bd84dcdc970ece
Mufaqar Islam <mufaqar@gmail.com>
2023-11-04 19:01:41 +0500

    Update sanity.cli.js

--- sanity.cli.js
+++ sanity.cli.js
@@ -3,7 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '5x5wnf9u',
-    dataset: 'production'
-    
+    dataset: 'production  
   }
 })
