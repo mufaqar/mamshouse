3b5d95003d5b28e88659b14f18c6a777500351cd
Mufaqar Islam <mufaqar@gmail.com>
2025-01-10 08:36:50 +0500

    Update store.jsx

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,8 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import openBookingTabSlice from "./features/BookingTabSlice/openBookingTabSlice";
 import TotalBookingDays from "./features/BookingTabSlice/TotalBookingDays"
-
-
 const store = configureStore({
     reducer:{
         openBookingTab : openBookingTabSlice.reducer,
